29c12b9160be74fb0beff3380d7519026508479f
Luiz Silva <luizsilva.ee@gmail.com>
2021-04-03 23:01:33 -0300

    [feature] Criação de contatos

--- app/Http/Livewire/ContactCreate.php
+++ app/Http/Livewire/ContactCreate.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Contact;
 use Livewire\Component;
 
 class ContactCreate extends Component
 {
+    public $name;
+    public $email;
+    public $phone;
+
+    protected $rules = [
+        'name' => 'required',
+        'email' => 'required',
+        'phone' => 'required',
+    ];
+
+    public function create()
+    {
+        $this->validate();
+        Contact::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone
+            ]);
+
+        $this->name = $this->phone = $this->email = null;
+    }
+
     public function render()
     {
         return view('livewire.contact-create');
